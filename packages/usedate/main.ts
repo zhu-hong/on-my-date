e3c851567321b395c905f0ec45e8e85876b25ff9
zhu-hong <zhxhyy@qq.com>
2022-07-11 21:53:40 +0800

    pkg datematrix rename usedate

--- packages/usedate/main.ts
+++ packages/usedate/main.ts
@@ -5,6 +5,43 @@ let year = date.getFullYear()
 let month = date.getMonth() + 1
 let use_monday_start = (document.getElementById('start_monday') as HTMLInputElement).checked
 
+const yearHost = document.getElementById('year') as HTMLInputElement
+
+yearHost.value = year
+
+yearHost.addEventListener('change', (e) => {
+  const value = Number(e.target.value)
+  if(isNaN(value) || value > 2100 || value < 1990) {
+    e.target.value = year
+    return
+  }
+
+  year = value
+
+  useCalendar(year, month, use_monday_start)
+})
+
+const monthHost = document.getElementById('month') as HTMLInputElement
+
+monthHost.value = month
+
+monthHost.addEventListener('change', (e) => {
+  const value = Number(e.target.value)
+  if(isNaN(value) || value > 12 || value < 1) {
+    e.target.value = month
+    return
+  }
+
+  if(year === 1990 && value < 3) {
+    e.target.value = month
+    return
+  }
+
+  month = value
+
+  useCalendar(year, month, use_monday_start)
+})
+
 const container = document.querySelector('main')
 
 const useCalendar = (year: number, month: number, use_monday_start: boolean) => {
@@ -15,9 +52,6 @@ const useCalendar = (year: number, month: number, use_monday_start: boolean) => 
 
   useDateMatrix(year, month, use_monday_start).map((week) => {
     week = week.map((day) => {
-      // @ts-ignore
-      // day['day'] = '周' + ['日', '一', '二', '三', '四', '五', '六'][new Date(day.dateStr).getDay()]
-
       day.lunar = useLunar(day.year, day.month, day.date)
   
       return day
@@ -45,6 +79,8 @@ const useCalendar = (year: number, month: number, use_monday_start: boolean) => 
 useCalendar(year, month, use_monday_start)
 
 document.getElementById('minus')?.addEventListener('click', () => {
+  if(year === 1990 && month === 3) return
+
   month--
 
   if(month === 0) {
@@ -52,16 +88,24 @@ document.getElementById('minus')?.addEventListener('click', () => {
     month = 12
   }
 
+  yearHost.value = year
+  monthHost.value = month
+
   useCalendar(year, month, use_monday_start)
 })
 
 document.getElementById('add')?.addEventListener('click', () => {
+  if(year === 2100 && month === 11) return
+
   month++
 
   if(month > 12) {
     year++
     month = 1
   }
+
+  yearHost.value = year
+  monthHost.value = month
 
   useCalendar(year, month, use_monday_start)
 })
