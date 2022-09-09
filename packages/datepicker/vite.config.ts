a184951940f36403a707f9ec60e3f712ec26c9ee
zhu-hong <zhxhyy@qq.com>
2022-09-09 21:44:51 +0800

    build cfg

--- packages/datepicker/vite.config.ts
+++ packages/datepicker/vite.config.ts
@@ -4,38 +4,23 @@ import unocss from 'unocss/vite'
 import { presetWind } from 'unocss'
 import { createVuePlugin } from 'vite-plugin-vue2'
 
-export default defineConfig((ctx) => {
-  if(ctx.mode === 'lib') {
-    return {
-      plugins: [
-        createVuePlugin(),
-        unocss({
-          presets: [
-            presetWind(),
-          ],
-        }),
+export default defineConfig({
+  plugins: [
+    createVuePlugin(),
+    unocss({
+      presets: [
+        presetWind(),
       ],
-      build: {
-        lib: {
-          entry: resolve(__dirname, 'src/components/index'),
-          formats: ['es'],
-          fileName: 'index',
-        },
-        rollupOptions: {
-          external: ['@zhu-hong/usedate', 'vue', 'element-ui'],
-        },
-      },
-    }
-  } else {
-    return {
-      plugins: [
-        createVuePlugin(),
-        unocss({
-          presets: [
-            presetWind(),
-          ],
-        }),
-      ],
-    }
-  }
+    }),
+  ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/components/index'),
+      formats: ['es'],
+      fileName: 'index',
+    },
+    rollupOptions: {
+      external: ['@zhu-hong/usedate', 'vue', 'element-ui'],
+    },
+  },
 })
